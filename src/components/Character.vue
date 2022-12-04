40382238b9f4a7bd7a9fd18495c144322061a8bf
appert <scoreur@gmail.com>
2022-12-04 11:23:17 +0100

    ajout de character

---
+++ src/components/Character.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>Hello this is character.vue</h3>
+    <p>{{ this.$route.params.id}}</p>
+  </div>
+</template>
+
+<script >
+export default {
+    name: 'character'
+}
+
+</script>
+
+
+<style>
+
+</style>
--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -4,7 +4,8 @@
 
     <ul>
       <li v-for="character in characters">
-      {{character.name}}
+        <router-link :to="{name:'character', params: { id: character.id}}">{{character.name}}</router-link>
+      
       </li>
 
 
