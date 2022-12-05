e93f06a3568d4288594dfe1d3f68edb869839070
appert <scoreur@gmail.com>
2022-12-05 12:38:57 +0100

    ajout du css

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
-    <h3>Hello this is character.vue</h3>
-    <p>{{ this.$route.params.id}}</p>
-    <ul>
-      <li v-for="char in character">
-        {{char.name}}
-        {{char.description}}
-      </li>
-    </ul>
-    <img :src="url" alt="">
+
+    <div class="flex-container">
+      <div class="flex" v-for="char in character">
+        <h3>
+          {{char.name}}
+        </h3>
+        <p>
+          {{char.description}}
+        </p>
+      
+
+      </div>
+
+      <div class="flex2">
+        <img :src="url" alt="" class="char-img">
+
+      </div>
+    </div>
+    <router-link to="/">
+      <button type="button" name="button" class="btn-back">Back</button>
+
+    </router-link>
+
   </div>
 </template>
 
@@ -24,48 +38,58 @@ export default {
         size:'standard_large.jpg',
       }
     },
-
     mounted() {
       this.getCharacter()
     },
-
-
     methods: {
       getCharacter: function() {
-
         var characterId = this.$route.params.id
-
         axios.get(`http://gateway.marvel.com/v1/public/characters/${characterId}?apikey=${public_key}`)
         .then((result) =>{
           console.log(result)
           result.data.data.results.forEach((item) => {
             this.character.push(item)
-
             this.url = `${item.thumbnail.path}/${this.size}`
-
-
-
-
           });
-
-
-
         })
         .catch((error) =>{
           console.log(error)
         })
-
-
-
-
       }
-
     }
 }
-
 </script>
 
 
 <style>
+.flex-container {
+  margin: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
+}
+.flex {
+  flex: 50%;
+  text-align: right;
+}
+
+.flex2 {
+  flex: 50%;
+}
+.char-img {
+  width: 50%;
+  border-radius: 10px;
+
+}
+.btn-back {
+  width: 200px;
+  padding: 15px;
+  border-radius: 25px;
+  background-color: transparent;
+  font-size: 20px;
+  margin-bottom: 100px;
+  cursor: pointer;
+
+}
 </style>
--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,44 +1,37 @@
 <template>
-  <div>
-    <h1>characters vue</h1>
+<div>
 
-    <ul>
-      <li v-for="character in characters">
-        <router-link :to="{name:'character', params: { id: character.id}}">{{character.name}}</router-link>
+  <div class="card-container">
+    <div class="card" v-for="character in characters">
+      <h3>{{character.name}}</h3>
+      <router-link :to="{name:'character', params: { id: character.id}}">
+        <button type="button" name="button" class="btn-view">View</button>
       
-      </li>
-
-
-    </ul>
-
-
+      </router-link>
+    </div>
+   
   </div>
+
+</div>
+  
+
 </template>
 
-<style >
-
-</style>
 
 <script >
 import {public_key, secret_key} from '../marvel';
 import axios from 'axios';
-
 export default {
     name: 'Characters',
     data() {
       return {
         characters: []
       }
-
     },
-
     mounted () {
       this.getCharacters()
-
     },
-
 /*obtention des data avec key, puis boucle pr les classer */
-
     methods: {
       getCharacters: function() {
         axios.get(`http://gateway.marvel.com/v1/public/characters?apikey=${public_key}`)
@@ -46,7 +39,6 @@ export default {
         
             result.data.data.results.forEach((item)=>{
               console.log(item)
-
               this.characters.push(item)
             })
           })
@@ -56,5 +48,33 @@ export default {
         }
     }
 }
-
 </script>
+
+<style >
+.card-container {
+  margin-left: 310px;
+  display: grid;
+  grid-template-columns: 200px 200px 200px;
+  grid-gap: 10px
+}
+.card {
+  text-align: center;
+  background-color: #777;
+  min-height: 200px;
+  max-width: 200px;
+}
+.card h3 {
+  color: #fff;
+}
+.btn-view {
+  padding: 10px;
+  margin-top: 50px;
+  border-radius: 15px;
+  width: 120px;
+  background-color: transparent;
+  color: #fff;
+  font-size: 12px;
+  font-weight: bold;
+  cursor: pointer;
+}
+</style>
